6dd42e4bab5415f55ebc8750ce070b8996e74e92
Ahmed Shoshan <shoshan@Ahmeds-MacBook-Air.local>
2026-03-14 17:16:18 +0200

    enhance nav UX

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -11,6 +11,7 @@ export const CustomNavbar = () => {
   const navigate = useNavigate();
   const cartItems = useSelector((state) => state.cart.items);
   const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     if (location.pathname !== "/") {
@@ -57,17 +58,25 @@ export const CustomNavbar = () => {
   };
 
   return (
-    <Navbar bg="light" variant="light" expand="lg" fixed="top">
+    <Navbar
+      bg="light"
+      variant="light"
+      expand="lg"
+      fixed="top"
+      expanded={expanded}
+      className="shadow-sm"
+    >
       <Container>
         <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center">
           <Logo size={48} />
         </Navbar.Brand>
 
-        <Navbar.Toggle />
+        <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse>
           <Nav className="ms-auto">
             <Nav.Link
               onClick={() => {
+                setExpanded(false);
                 if (window.location.pathname !== "/") {
                   navigate("/");
                 } else {
@@ -78,24 +87,36 @@ export const CustomNavbar = () => {
               Home
             </Nav.Link>
 
-            <Nav.Link as={NavLink} to="/products">
+            <Nav.Link
+              as={NavLink}
+              to="/products"
+              onClick={() => setExpanded(false)}
+            >
               Products
             </Nav.Link>
 
             <Nav.Link
-              onClick={() => scrollToSection("about-section")}
+              onClick={() => {
+                setExpanded(false);
+                scrollToSection("about-section");
+              }}
               className={activeSection === "about-section" ? "active" : ""}
             >
               About Us
             </Nav.Link>
 
             <Nav.Link
-              onClick={() => scrollToSection("contact-section")}
+              onClick={() => {
+                setExpanded(false);
+                scrollToSection("contact-section");
+              }}
               className={activeSection === "contact-section" ? "active" : ""}
             >
               Contact Us
             </Nav.Link>
             <Nav.Link as={Link} to="/cart" className="cart-link">
+              {" "}
+              onClick={() => setExpanded(false)}
               <span className="cart-icon-wrapper">
                 <FaShoppingCart size={22} />
                 {cartCount > 0 && (
